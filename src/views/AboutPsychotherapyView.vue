38fb69f378a7182ecd5768200010da25cffac8bb
kotnikd3 <denis.kotnik@gmail.com>
2026-06-05 18:13:05 +0200

    feat: add text about psychotherapy

--- src/views/AboutPsychotherapyView.vue
+++ src/views/AboutPsychotherapyView.vue
@@ -4,9 +4,12 @@
             <div class="columns is-vcentered">
                 <div class="column is-golden-main content">
                     <h3 class="title is-5 has-text-primary">Kaj je psihodinamska psihoterapija?</h3>
-                    <p>
-                        Psihodinamska psihoterapija je <b>zdravljenje s pogovorom</b>, ki temelji na ideji, da lahko nezavedne misli in občutki povzročajo težave, kot so anksioznost, depresija, slaba samopodoba in težave v odnosih. Eden od načinov kako se spoprijemamo z bolečimi ali težkimi mislimi in občutki je, da o njih ne razmišljamo, da jih ignoriramo — torej da jih potisnemo v <strong>nezavedno</strong>. Toda tudi če o njih ne razmišljamo, nezavedne misli in občutki še vedno vplivajo na to, kako mislimo, čutimo in se vedemo. Na primer, morda vam je težko čutiti jezo do nekoga, ki ga imate radi, saj menite, da tega ne bi smeli čutiti. Kljub temu pa lahko ta občutja še naprej vplivajo na vaš odnos. Psihodinamska psihoterapija omogoča prostor za razvoj novih, bolj zdravih načinov soočanja z omenjenimi težavami.
-                    </p>
+                    <div class="block">
+                        Psihodinamska psihoterapija je <b>zdravljenje s pogovorom</b>, ki temelji na ideji, da lahko nezavedne misli in občutki povzročajo težave, kot so anksioznost, depresija, slaba samopodoba in težave v odnosih. Je modificirana oblika <b>psihoanalize</b>, ki temelji na teoretskih osnovah psihoanalitičnega nauka: splošen in specialen nauk o <b>nevrozah</b>, na <b>psihoanalitični razvojni teoriji</b>, na nauku o <b>transferju</b>, <b>kontratransferju</b>, <b>obrambah</b> in <b>odporih</b>.
+                    </div>
+                    <div class="block">
+                        Eden od načinov kako se spoprijemamo z bolečimi ali težkimi mislimi in občutki je, da o njih ne razmišljamo, da jih ignoriramo — torej da jih potisnemo v <strong>nezavedno</strong>. Toda tudi če o njih ne razmišljamo, nezavedne misli in občutki še vedno vplivajo na to, kako mislimo, čutimo in se vedemo. Na primer, morda vam je težko čutiti jezo do nekoga, ki ga imate radi, saj menite, da tega ne bi smeli čutiti. Kljub temu pa lahko ta občutja še naprej vplivajo na vaš odnos. Psihodinamska psihoterapija omogoča prostor za razvoj novih, bolj zdravih načinov soočanja z omenjenimi težavami.
+                    </div>
                 </div>
                 <div class="column is-golden-side has-text-centered">
                     <img class="img-iceberg" src="@/assets/images/iceberg.svg" alt="Iceberg" style="width: 60%;">
